cf0a15572b512ab5f2a696b88a63fae5298135bb
Mateo <mateom.realtor@gmail.com>
2022-08-05 10:40:53 -0400

    Add Carousel feature

---
+++ sandbox.js
@@ -0,0 +1,28 @@
+let navBar = document.querySelector("#navBarSection")
+
+// Function that enables scrolling functionality
+window.addEventListener("scroll", function () {
+    const scrolled = window.scrollY;
+    if (scrolled === 100) {
+        navBar.classList.replace('navBarSection', 'hello')
+        console.log(navBar.classList)
+    }
+    console.log(scrolled)
+
+})
+
+// Function that enables Carousel functionality
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
