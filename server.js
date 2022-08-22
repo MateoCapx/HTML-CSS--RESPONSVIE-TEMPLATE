9e2a6451e98a3facaf98c39207535f20fe64ac0a
Mateo <mateom.realtor@gmail.com>
2022-08-22 10:41:46 -0400

    commiting

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const client = require('twilio')(accountSid, authToken);
 const PORT = process.env.PORT || 3001;
 const app = express();
 // const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('/routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
